b92761a9d32065a72648ee50e8d6ff6b88e489f8
Lala5th <lala5th@gmail.com>
2021-03-05 06:12:36 +0000

    Added normalised

--- plotEDepDiffMultSingleNorm.py
+++ plotEDepDiffMultSingleNorm.py
@@ -11,7 +11,8 @@ cmap = cm.get_cmap('gnuplot')
 
 dirRun = args[1]
 dirRef = args[2]
-id = int(args[3])
+dirErr = args[3]
+id = int(args[4])
 
 ERef = np.load(dirRef + '/InitialEnergy.npy')['0']
 y = np.load(dirRef + '/y.npy')['0']
@@ -26,22 +27,39 @@ data = np.reshape(np.load(dirRun + '/DepositedEnergy.npy'),(len(ECrits),len(y),l
 
 layers = np.array([np.sum(ref['1'][i,:,:],axis = 0) for i in range(len(ERef))])
 
+
+Err = np.reshape(np.load(dirErr + '/DepositedEnergy.npy'),(len(ERef),len(y),len(z)))['1']
+
+Err = np.sum(Err,axis=1)
+Err[Err < 0] = 0
+Err = np.sqrt(Err)
+
 plt.ion()
 plt.figure()
+plt.plot(z,layers[id])
+
+fig, (ax1, ax2) = plt.subplots(2,1,sharex=True)
 new_ERef = (ERef[1:] + ERef[:-1])/2
 new_ERef = np.append(new_ERef,ERef[-1]+10)
 new_ERef = np.insert(new_ERef,0,0)
 l = np.histogram(EDist[id],bins=new_ERef)
 l_err = np.sqrt(l[0])
-EDep = np.zeros(len(z))
+geantE = np.sqrt(np.sum((Err.T*np.sqrt(l[0]))**2,axis=1))
+
 EDep = np.sum((l[0])*layers.T,axis=1)
-EDep_d = np.sum((1.96*l_err)*layers.T,axis=1)/len(ECrits)
-EDep_n = EDep - EDep_d
-EDep_p = EDep + EDep_d
-plincomb, = plt.plot(z,EDep/np.max(EDep),label='Linear combination')
-psim, = plt.plot(z,np.sum(data[id],axis=0)/np.max(np.sum(data[id],axis=0)),label='Simulated')
-plt.fill_between(z,EDep_n/np.max(EDep),EDep_p/np.max(EDep),label='95% CI',alpha=0.2)
-plt.legend()
-plt.ylabel('E$_{norm}$ [a.u.]')
-plt.xlabel('Longitudinal crystal ID')
+
+EDep_d = 1.96*np.sqrt(np.max(l_err*layers.T,axis=1)**2 + geantE**2)/np.max(EDep)
+EDep /= np.max(EDep)
+d = np.sum(data[id],axis=0)
+d /= np.max(d)
+ax1.plot(z,EDep,label='Linear combination')
+ax1.plot(z,d,label='Simulated')
+ax1.fill_between(z,EDep - EDep_d,EDep + EDep_d,label='95% CI',alpha=0.2)
+ax1.legend()
+ax1.set_ylabel('E [MeV]')
+ax2.set_ylabel('E [MeV]')
+ax2.set_xlabel('Longitudinal crystal ID')
+ax2.plot(z,d - EDep,label='Residual')
+ax2.fill_between(z,-EDep_d,EDep_d,label='95% CI',alpha=0.2)
+ax2.legend()
 plt.show()
