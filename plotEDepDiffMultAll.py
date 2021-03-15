0aa2c4402fb00e82f4db9f2b53179a79da88cdf7
Lala5th <lala5th@gmail.com>
2021-03-15 14:35:34 +0000

    Fixed RejSamp

--- plotEDepDiffMultAll.py
+++ plotEDepDiffMultAll.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cm
 from matplotlib.widgets import Slider
+from scipy.optimize import curve_fit
 
 def get_chi2(E, x, s):
     return np.sum(((E-x)/s)**2)
@@ -37,13 +38,25 @@ Err = np.reshape(np.load(dirErr + '/DepositedEnergy.npy'),(len(ERef),len(y),len(
 Err = np.sum(Err,axis=1)
 #Err[Err < 0] = 0
 Err = np.sqrt(Err)
-
-fig, (ax1, ax2) = plt.subplots(2,1,sharex=True)
+fig = plt.figure()
+ax0 = fig.add_subplot(3,1,1,yscale="log")
+ax2 = fig.add_subplot(3,1,3)
+ax1 = fig.add_subplot(3,1,2,sharex=ax2)
+plt.setp(ax1.get_xticklabels(),visible=False)
 new_ERef = (ERef[1:] + ERef[:-1])/2
 new_ERef = np.append(new_ERef,ERef[-1]+10)
 new_ERef = np.insert(new_ERef,0,0)
+ERef_diff = np.insert(ERef,0,0)
+ERef_diff = (new_ERef[1:] - new_ERef[:-1])
 l = np.histogram(EDist[id],bins=new_ERef)
 l_err = np.sqrt(l[0])
+ax0.plot(ERef,l[0]/ERef_diff)
+func = lambda x, A : A*x**(-2/3)*np.exp(-x/ECrits[id])
+fit,cov = curve_fit(func,ERef,l[0]/ERef_diff)
+#ax0.plot(ERef,func(ERef,*fit))
+ax0.set_ylabel("Adjusted count")
+ax0.set_xlabel("E [MeV]")
+ax0.set_ylim([1e-4,5*max(np.max(func(ERef,*fit)),np.max(l[0]/ERef_diff))])
 
 geantE = np.sqrt(np.sum((Err.T*l_err)**2,axis=1))
 EDep = np.sum((l[0])*layers.T,axis=1)
@@ -59,8 +72,8 @@ ax2.set_xlabel('Longitudinal crystal ID')
 ax2.plot(z,np.sum(data[id],axis=0) - EDep,label='Residual')
 ax2.fill_between(z,-EDep_d,EDep_d,label='95% CI',alpha=0.2)
 ax2.legend()
-plt.subplots_adjust(left=0.25, bottom=0.25)
-axE = plt.axes([0.25, 0.1, 0.65, 0.03])
+plt.subplots_adjust(left=0.2, bottom=0.2,hspace=0.5,top=0.9)
+axE = plt.axes([0.25, 0.05, 0.65, 0.03])
 sE = Slider(axE,'E$_{init}$ [MeV]',ECrits[0],ECrits[-1],valstep=ECrits[1]-ECrits[0],valinit=ECrits[0])
 
 def print_wrapped(func):
@@ -79,6 +92,15 @@ def update(val):
     geantE = np.sqrt(np.sum((Err.T*l_err)**2,axis=1))
     EDep = np.sum((l[0])*layers.T,axis=1)
     EDep_d = np.sqrt(np.sum((l_err*layers.T)**2,axis=1) + geantE**2)
+    ax0.clear()
+    ax0.set_yscale("log")
+    ax0.plot(ERef,l[0]/ERef_diff)
+    ax0.set_ylabel("Adjusted count")
+    ax0.set_xlabel("E [MeV]")
+    func = lambda x, A : A*x**(-2/3)*np.exp(-x/ECrits[Eid])
+    fit,cov = curve_fit(func,ERef,l[0]/ERef_diff)
+    #ax0.plot(ERef,func(ERef,*fit))
+    ax0.set_ylim([1e-4,5*max(np.max(func(ERef,*fit)),np.max(l[0]/ERef_diff))])
     ax1.clear()
     ax2.clear()
     ax1.plot(z,EDep,label='Linear combination')
